144aef9d732f4cf7f3e3deed82795c05dc6a6b19
Mihai Anghelin <mihai.anghelin@gmail.com>
2020-02-01 06:43:36 +0200

    Mouse collision with blocks

--- opengl2DLib/opengl2Dlib.cpp
+++ opengl2DLib/opengl2Dlib.cpp
@@ -503,7 +503,7 @@ namespace gl2d
 			cameraCenter.x = windowW / 2.0f;
 			cameraCenter.y = -windowH / 2.0f;
 
-			std::cout << cameraCenter.x << "\n";
+			//std::cout << cameraCenter.x << "\n";
 
 			v1 = scaleAroundPoint(v1, cameraCenter, currentCamera.zoom);
 			v2 = scaleAroundPoint(v2, cameraCenter, currentCamera.zoom);
